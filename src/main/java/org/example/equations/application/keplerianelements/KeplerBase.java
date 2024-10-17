ebfbf312a6a9efbefa7dfd722df4d7d54d18db5f
AkosseDeVamp <akossedevamp@gmail.com>
2024-10-17 17:53:53 +0100

    Streamlined the Abstract KeplerBase class

--- src/main/java/org/example/equations/application/keplerianelements/KeplerBase.java
+++ src/main/java/org/example/equations/application/keplerianelements/KeplerBase.java
@@ -8,31 +8,6 @@ public abstract class KeplerBase implements Kepler{
     protected double data;
 
     @Override
-    public KeplarianElement getType() {
-        return null;
-    }
-
-    @Override
-    public String getAsString() {
-        return null;
-    }
-
-    @Override
-    public void setFromString(String string) {
-
-    }
-
-    @Override
-    public String displayName() {
-        return null;
-    }
-
-    @Override
-    public String unitSI() {
-        return null;
-    }
-
-    @Override
     public double getData() {
         return 0;
     }
