c5b649ce7cccce2922584a0c7b3581bc6f74c28e
AkosseDeVamp <akossedevamp@gmail.com>
2024-10-27 12:24:56 +0000

    created the inclination keplerianelement

--- src/main/java/org/example/equations/application/keplerianelements/Inclination.java
+++ src/main/java/org/example/equations/application/keplerianelements/Inclination.java
@@ -1,5 +1,46 @@
 package org.example.equations.application.keplerianelements;
 
-public class Inclination {
+import lombok.Data;
 
+@Data
+public class Inclination implements Kepler {
+
+    private double data;
+
+    @Override
+    public KeplerEnums getType() {
+        return KeplerEnums.INCLINATION;
+    }
+
+    @Override
+    public String getAsString() {
+        double degrees = Math.toDegrees(data);
+        return String.format("%.2f",degrees);
+    }
+
+    @Override
+    public void setFromString(String string) {
+        double degrees = Double.parseDouble(string);
+        data = Math.toRadians(degrees);
+    }
+
+    @Override
+    public String displayName() {
+        return "Inclination";
+    }
+
+    @Override
+    public String unitSI() {
+        return "°";
+    }
+
+    @Override
+    public double getData() {
+        return data;
+    }
+
+    @Override
+    public void setData(double data) {
+        this.data = data;
+    }
 }
