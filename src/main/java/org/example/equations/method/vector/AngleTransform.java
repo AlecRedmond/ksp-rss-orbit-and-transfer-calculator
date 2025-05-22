779100010502712e84d7502c66b69b40eb2c2f6c
AkosseDeVamp <akossedevamp@gmail.com>
2025-05-22 10:26:59 +0100

    added descriptions to methods in the AngleTransform class

--- src/main/java/org/example/equations/method/vector/AngleTransform.java
+++ src/main/java/org/example/equations/method/vector/AngleTransform.java
@@ -1,17 +1,15 @@
 package org.example.equations.method.vector;
 
+import static org.apache.commons.math3.geometry.euclidean.threed.RotationConvention.FRAME_TRANSFORM;
+import static org.apache.commons.math3.geometry.euclidean.threed.RotationOrder.ZXZ;
+import static org.example.equations.application.keplerianelements.Kepler.KeplerEnums.*;
+
+import java.util.Optional;
 import org.apache.commons.math3.geometry.euclidean.threed.Line;
 import org.apache.commons.math3.geometry.euclidean.threed.Plane;
 import org.apache.commons.math3.geometry.euclidean.threed.Rotation;
 import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
 import org.example.equations.application.Orbit;
-
-import java.util.Optional;
-
-import static org.apache.commons.math3.geometry.euclidean.threed.RotationConvention.FRAME_TRANSFORM;
-import static org.apache.commons.math3.geometry.euclidean.threed.RotationConvention.VECTOR_OPERATOR;
-import static org.apache.commons.math3.geometry.euclidean.threed.RotationOrder.ZXZ;
-import static org.example.equations.application.keplerianelements.Kepler.KeplerEnums.*;
 
 public class AngleTransform {
   private static final double TOLERANCE = 1e-6;
@@ -23,35 +21,38 @@ public class AngleTransform {
   private double rightAscension = 0;
   private Rotation rotation;
 
-  public AngleTransform setVelocityAngle(Vector3D velocity, Vector3D radius){
-    velocityAngle = Vector3D.angle(velocity,radius);
+  public AngleTransform setVelocityAngle(Vector3D velocity, Vector3D radius) {
+    velocityAngle = Vector3D.angle(velocity, radius);
     return this;
   }
 
-  public AngleTransform setAnomalyAngle(double trueAnomaly){
+  public AngleTransform setAnomalyAngle(double trueAnomaly) {
     anomalyAngle = trueAnomaly;
     return this;
   }
 
-  public AngleTransform setOrbitAngles(Orbit orbit){
+  public AngleTransform setOrbitAngles(Orbit orbit) {
     rightAscension = orbit.getDataFor(RIGHT_ASCENSION);
     inclination = orbit.getDataFor(INCLINATION);
     argumentPE = orbit.getDataFor(ARGUMENT_PE);
     return this;
   }
 
-  public Rotation getToMotionInitializer(){
-    return new Rotation(Z_AXIS,velocityAngle,FRAME_TRANSFORM);
+  /**
+   * Returns a rotation to the Motion (Velocity-Centric) frame, where the X-Axis points in the
+   * direction of the current tangential Velocity. The Y axis will describe the "Radial" direction
+   * vector, and the Z axis the Orbital Normal.
+   */
+  public Rotation getToMotionInitializer() {
+    return new Rotation(Z_AXIS, velocityAngle, FRAME_TRANSFORM);
   }
 
-  public Rotation getInertialFromMotion(){
+  /**
+   * Returns the angle to the body-centric Inertial frame from the Motion (Velocity-centric) frame.
+   */
+  public Rotation getInertialFromMotion() {
     var finalZAngle = argumentPE + anomalyAngle + velocityAngle;
-    return new Rotation(ZXZ,FRAME_TRANSFORM,-finalZAngle,-inclination,-rightAscension);
-  }
-
-  private static Rotation getInertialFromPlanar(
-      double rightAscension, double inclination, double argumentPE) {
-    return new Rotation(ZXZ, FRAME_TRANSFORM, -argumentPE, -inclination, -rightAscension);
+    return new Rotation(ZXZ, FRAME_TRANSFORM, -finalZAngle, -inclination, -rightAscension);
   }
 
   /**
@@ -75,5 +76,10 @@ public class AngleTransform {
     Rotation planarToInertial = getInertialFromPlanar(rightAscension, inclination, argumentPE);
     Vector3D normalVector = planarToInertial.applyTo(Z_AXIS);
     return new Plane(normalVector, TOLERANCE);
+  }
+
+  private static Rotation getInertialFromPlanar(
+      double rightAscension, double inclination, double argumentPE) {
+    return new Rotation(ZXZ, FRAME_TRANSFORM, -argumentPE, -inclination, -rightAscension);
   }
 }
