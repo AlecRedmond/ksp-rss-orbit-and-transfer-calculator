7cccf255c229d5740cebc9813a889986d4311d10
Alec Redmond <akossedevamp@gmail.com>
2025-06-04 15:17:05 +0100

    Specified Lombok Version to allow build to complete.

--- src/main/java/org/artools/orbitcalculator/controller/BodyPositionController.java
+++ src/main/java/org/artools/orbitcalculator/controller/BodyPositionController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-public class SolarSystemController {
+public class BodyPositionController {
   @Autowired private BodyPositionService service;
 
   // Save
